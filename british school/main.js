9cf488a798c26e48fe1834d6d64d5ea9c80d0a2a
Khalid tajelsir <105322070+uzisin@users.noreply.github.com>
2022-10-08 03:19:33 -0700

    Add files via upload

---
+++ british school/main.js
@@ -0,0 +1,21 @@
+
+
+	var	username = "11";
+	var	password = "11"
+	
+
+
+function getInfo() {
+	var username = document.getElementById('user').value
+	var password = document.getElementById('pass').value
+
+		if(user == username && pass == password) {
+			console.log(username + " is logged in!!!")
+			
+		}
+	
+    else{
+	console.log("incorrect username or password")
+        }
+
+    }
